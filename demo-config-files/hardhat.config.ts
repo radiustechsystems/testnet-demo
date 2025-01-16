be9891aa8734c780fc0b04781c7df47977b36385
Gert-Jaap Glasbergen <gertjaap@wadagso.com>
2025-01-16 22:24:40 +0100

    hardhat config: set required confs to 0 to prevent hanging on deploys

--- demo-config-files/hardhat.config.ts
+++ demo-config-files/hardhat.config.ts
@@ -3,20 +3,24 @@ import "@nomicfoundation/hardhat-ignition-ethers";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
- solidity: {
-   compilers: [
-     {
-       version: "0.8.28",
-     },
-   ],
- },
- defaultNetwork: "radius",
- networks: {
-   radius: {
-     url: "<your-agent-rpc-url>",
-     accounts: ["<your-private-key>"]
-   }
- }
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+      },
+    ],
+  },
+  defaultNetwork: "radius",
+  networks: {
+    radius: {
+      url: "<your-agent-rpc-url>",
+      accounts: ["<your-private-key>"],
+     
+    }
+  },
+  ignition: {
+    requiredConfirmations: 1,
+  },
 };
 
 export default config;
